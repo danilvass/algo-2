6c71fa11f2f02d6a3bf1859278f0b1d7e47497d8
Васякин Данил Евгеньевич <Danil.Vassyakin@kaspi.kz>
2022-12-14 22:42:11 +0600

    no message

--- src/BST.java
+++ src/BST.java
@@ -2,16 +2,14 @@ import java.io.*;
 import java.util.*;
 
 
-class BSTNode<T>
-{
+class BSTNode<T> {
     public int NodeKey; // ключ узла
     public T NodeValue; // значение в узле
     public BSTNode<T> Parent; // родитель или null для корня
     public BSTNode<T> LeftChild; // левый потомок
     public BSTNode<T> RightChild; // правый потомок	
 
-    public BSTNode(int key, T val, BSTNode<T> parent)
-    {
+    public BSTNode(int key, T val, BSTNode<T> parent) {
         NodeKey = key;
         NodeValue = val;
         Parent = parent;
@@ -21,8 +19,7 @@ class BSTNode<T>
 }
 
 // промежуточный результат поиска
-class BSTFind<T>
-{
+class BSTFind<T> {
     // null если в дереве вообще нету узлов
     public BSTNode<T> Node;
 
@@ -32,20 +29,19 @@ class BSTFind<T>
     // true, если родительскому узлу надо добавить новый левым
     public boolean ToLeft = false;
 
-    public BSTFind() { Node = null; }
+    public BSTFind() {
+        Node = null;
+    }
 }
 
-class BST<T>
-{
+class BST<T> {
     BSTNode<T> Root; // корень дерева, или null
 
-    public BST(BSTNode<T> node)
-    {
+    public BST(BSTNode<T> node) {
         Root = node;
     }
 
-    public BSTFind<T> FindNodeByKey(int key)
-    {
+    public BSTFind<T> FindNodeByKey(int key) {
         BSTFind<T> find = new BSTFind<T>();
         BSTNode<T> node = Root;
         while (node != null) {
@@ -67,14 +63,15 @@ class BST<T>
         return find;
     }
 
-    public boolean AddKeyValue(int key, T val)
-    {
+    public boolean AddKeyValue(int key, T val) {
         if (Root == null) {
             Root = new BSTNode<>(key, val, null);
             return true;
         }
         BSTFind<T> find = FindNodeByKey(key);
-        if (find.NodeHasKey) { return false; }
+        if (find.NodeHasKey) {
+            return false;
+        }
         if (find.ToLeft) {
             find.Node.LeftChild = new BSTNode<>(key, val, find.Node);
         } else {
@@ -83,8 +80,7 @@ class BST<T>
         return true;
     }
 
-    public BSTNode<T> FinMinMax(BSTNode<T> FromNode, boolean FindMax)
-    {
+    public BSTNode<T> FinMinMax(BSTNode<T> FromNode, boolean FindMax) {
         BSTNode<T> node = FromNode;
         BSTNode<T> foundNode = node;
         while (node != null) {
@@ -94,63 +90,60 @@ class BST<T>
         return foundNode;
     }
 
-    public boolean DeleteNodeByKey(int key)
-    {
-        BSTFind<T> nodeToFind = FindNodeByKey(key);
-        if (!nodeToFind.NodeHasKey || nodeToFind.Node == null) {
-            return false;
-        }
-
-        BSTNode<T> nodeToDelete = nodeToFind.Node;
-        BSTNode<T> oldParent = nodeToDelete.Parent;
-        if (nodeToDelete.Parent == null) {
-            changeParentInNode(nodeToDelete.LeftChild, nodeToDelete.RightChild, true);
-            Root = nodeToDelete.RightChild;
-            return true;
-        }
-
-        boolean isParentLeftChild = nodeToFind.ToLeft;
-
-        if (!hasAnyChild(nodeToDelete)) {
-            if (isParentLeftChild) {
-                oldParent.LeftChild = null;
-            } else {
-                oldParent.RightChild = null;
-            }
-            return true;
-        }
-
-        BSTNode<T> successor = FinMinMax(nodeToDelete.RightChild, false);
-        if (!hasAnyChild(successor)) {
-            changeParentInNode(nodeToDelete.LeftChild, successor, true);
-            changeParentInNode(successor, oldParent, isParentLeftChild);
-        } else if (successor.RightChild != null) {
-            BSTNode<T> right = successor.RightChild;
-            BSTNode<T> successorParent = successor.Parent;
-            changeParentInNode(right, successorParent, isParentLeftChild);
-
-            if (isParentLeftChild) {
-                oldParent.LeftChild = successor;
-            } else {
-                oldParent.RightChild = successor;
-            }
-        }
-
-        return true;
+    //TODO: удалить этот костыль
+    private class DeletionResult {
+        boolean isDeleted = false;
     }
 
-    private void changeParentInNode(BSTNode<T> node, BSTNode<T> newParent, boolean isLeft) {
-        if (node == null || newParent == null) return;
-        node.Parent = newParent;
-        if (isLeft) {
-            newParent.LeftChild = node;
+    public boolean DeleteNodeByKey(int key) {
+        DeletionResult deletionResult = new DeletionResult();
+        Root = deleteNode(Root, key, deletionResult);
+        return deletionResult.isDeleted;
+    }
+
+    public BSTNode<T> deleteNode(BSTNode<T> root, int key, DeletionResult deletionResult) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key > root.NodeKey) {
+            root.RightChild = deleteNode(root.RightChild, key, deletionResult);
+        } else if (key < root.NodeKey) {
+            root.LeftChild = deleteNode(root.LeftChild, key, deletionResult);
         } else {
-            newParent.RightChild = node;
+            deletionResult.isDeleted = true;
+
+            if (root.LeftChild == null && root.RightChild == null) {
+                root = null;
+            } else if (root.RightChild != null) {
+                root.NodeKey = successor(root);
+                root.RightChild = deleteNode(root.RightChild, root.NodeKey, deletionResult);
+            } else {
+                root.NodeKey = predecessor(root);
+                root.LeftChild = deleteNode(root.LeftChild, root.NodeKey, deletionResult);
+            }
         }
+
+        return root;
     }
 
-    public int Count()
-    {
+    private int successor(BSTNode<T> root) {
+        root = root.RightChild;
+        while (root.LeftChild != null) {
+            root = root.LeftChild;
+        }
+        return root.NodeKey;
+    }
+
+    private int predecessor(BSTNode<T> root) {
+        root = root.LeftChild;
+        while (root.RightChild != null) {
+            root = root.RightChild;
+        }
+        return root.NodeKey;
+    }
+
+    public int Count() {
         return getCount(Root);
     }
 
@@ -159,9 +152,5 @@ class BST<T>
         int count = getCount(node.LeftChild) + getCount(node.RightChild);
         return count + 1;
     }
-
-    private boolean hasAnyChild(BSTNode<T> node) {
-        return node.LeftChild != null || node.RightChild != null;
-    }
-
+    
 }
--- src/BSTTest.java
+++ src/BSTTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -49,37 +52,75 @@ class BSTTest {
         sut.AddKeyValue(12, "12");
         sut.AddKeyValue(15, "15");
 
+        assertEquals(5, sut.Count());
         assertNotNull(sut.FindNodeByKey(12).Node.RightChild);
         sut.DeleteNodeByKey(15);
+        assertEquals(4, sut.Count());
         assertNull(sut.FindNodeByKey(12).Node.RightChild);
 
         sut.AddKeyValue(15, "15");
+        assertEquals(5, sut.Count());
+
         sut.AddKeyValue(13, "13");
+        assertEquals(6, sut.Count());
+
         sut.AddKeyValue(16, "16");
+        assertEquals(7, sut.Count());
+
         sut.AddKeyValue(14, "14");
+        assertEquals(8, sut.Count());
 
         assertEquals(sut.FindNodeByKey(13).Node.RightChild.NodeKey, 14);
 
-
         assertTrue(sut.DeleteNodeByKey(13));
+        assertEquals(7, sut.Count());
 
         assertFalse(sut.DeleteNodeByKey(13));
+        assertEquals(7, sut.Count());
 
         assertFalse(sut.FindNodeByKey(13).NodeHasKey);
         assertEquals(sut.FindNodeByKey(15).Node.LeftChild.NodeKey, 14);
 
 
         assertTrue(sut.DeleteNodeByKey(15));
+        assertEquals(6, sut.Count());
         System.out.println(sut);
 
         assertEquals(6, sut.Count());
     }
 
     @Test
+    void test_complex() {
+        BST<String> sut = new BST<String>(null);
+
+        Random rd = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int x = rd.nextInt(100);
+            if (i > 0 && !list.contains(x)) {
+                list.add(x);
+            }
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            sut.AddKeyValue(list.get(i), list.get(i).toString());
+            assertTrue(sut.FindNodeByKey(list.get(i)).NodeHasKey);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            assertTrue(sut.DeleteNodeByKey(list.get(i)));
+            assertEquals(list.size() - i - 1, sut.Count());
+            assertFalse(sut.DeleteNodeByKey(list.get(i)));
+        }
+    }
+
+    @Test
     void test_count() {
         BSTNode<String> root = new BSTNode<String>(3,"3",null);
-        BST<String> sut = new BST<String>(root);
+        BST<String> sut = new BST<String>(null);
+        assertEquals(0, sut.Count());
+        sut.AddKeyValue(3, "3");
         assertEquals(1, sut.Count());
+
         sut.AddKeyValue(4, "4");
         assertEquals(2, sut.Count());
         sut.AddKeyValue(7, "7");
