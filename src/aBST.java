2f79e6ebde03f0b05e28d7bd037e5ea729d2060b
Васякин Данил Евгеньевич <Danil.Vassyakin@kaspi.kz>
2022-12-27 18:45:47 +0600

    feat: add aBST implementation

--- src/aBST.java
+++ src/aBST.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class aBST {
+import java.util.*;
+
+class aBST
+{
+    public Integer Tree [];
+
+    public aBST(int depth)
+    {
+        int tree_size = (int) (Math.pow(2, depth) - 1);
+        Tree = new Integer[ tree_size ];
+        for(int i=0; i<tree_size; i++) Tree[i] = null;
+    }
+
+    public Integer FindKeyIndex(int key)
+    {
+        Integer index = 0;
+        while (index < Tree.length) {
+            if (Tree[index] == null) { return (index * -1); }
+            if (Tree[index] == key) { return index; }
+            if (key >= Tree[index]) {
+                index = rightIndexForChild(index);
+            } else {
+                index = leftIndexForChild(index);
+            }
+            if (index == null) { return null; }
+        }
+        return null;
+    }
+
+    public int AddKey(int key)
+    {
+        Integer index = FindKeyIndex(key);
+        if (index == null) { return -1; }
+
+        if (index <= 0) {
+            Tree[index * -1] = key;
+            return index * -1;
+        }
+        return index;
+    }
+
+    private Integer leftIndexForChild(int index) {
+        int childIndex = (2 * index) + 1;
+        if (childIndex >= Tree.length) return null;
+        return childIndex;
+    }
+
+    private Integer rightIndexForChild(int index) {
+        int childIndex = (2 * index) + 2;
+        if (childIndex >= Tree.length) return null;
+        return childIndex;
+    }
+
 }
--- src/aBSTTest.java
+++ src/aBSTTest.java
@@ -1,4 +1,58 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class aBSTTest {
-  
+
+    @Test
+    void test_init() {
+        assertEquals(1, new aBST(1).Tree.length);
+        assertEquals(3, new aBST(2).Tree.length);
+        assertEquals(7, new aBST(3).Tree.length);
+        assertEquals(15, new aBST(4).Tree.length);
+        assertEquals(31, new aBST(5).Tree.length);
+    }
+
+    @Test
+    void test_find() {
+        aBST sut = new aBST(3);
+        sut.Tree = new Integer[] { 50, 25, 75, null, null, null, null };
+        assertEquals(0, sut.FindKeyIndex(50));
+        assertEquals(1, sut.FindKeyIndex(25));
+        assertEquals(2, sut.FindKeyIndex(75));
+        assertEquals(-3, sut.FindKeyIndex(20));
+        assertEquals(-3, sut.FindKeyIndex(24));
+        assertEquals(-4, sut.FindKeyIndex(26));
+
+        sut.Tree = new Integer[] { 50, 25, 75, 20, 30, 60, 80 };
+        assertEquals(0, sut.FindKeyIndex(50));
+        assertEquals(1, sut.FindKeyIndex(25));
+        assertEquals(2, sut.FindKeyIndex(75));
+        assertEquals(3, sut.FindKeyIndex(20));
+        assertEquals(4, sut.FindKeyIndex(30));
+        assertEquals(5, sut.FindKeyIndex(60));
+        assertEquals(6, sut.FindKeyIndex(80));
+        assertNull(sut.FindKeyIndex(1));
+        assertNull(sut.FindKeyIndex(2));
+        assertNull(sut.FindKeyIndex(3));
+        assertNull(sut.FindKeyIndex(4));
+    }
+
+    @Test
+    void test_add() {
+        aBST sut = new aBST(3);
+        assertEquals(0, sut.AddKey(50));
+        assertEquals(1, sut.AddKey(25));
+        assertEquals(2, sut.AddKey(75));
+        assertEquals(1, sut.AddKey(25));
+        assertEquals(4, sut.AddKey(37));
+        assertEquals(5, sut.AddKey(62));
+        assertEquals(6, sut.AddKey(84));
+        assertEquals(3, sut.AddKey(20));
+        assertEquals(-1, sut.AddKey(51));
+        assertEquals(-1, sut.AddKey(0));
+        assertEquals(-1, sut.AddKey(1));
+        assertEquals(-1, sut.AddKey(100));
+    }
+
 }
